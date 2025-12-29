8df13380777698abd51d4e2af137a68bf6443fb6
Sanjay Kumar <sanjaykumar200599@gmail.com>
2025-12-29 23:21:59 +0530

    updated the schema

--- server/src/lib/auth.js
+++ server/src/lib/auth.js
@@ -1,7 +1,9 @@
+//Authentication
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db.js";
 import { deviceAuthorization } from "better-auth/plugins";
+
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
